adc4c5549ef255504ade5aa7abecd87b9f2bd5d0
william8012 <william8012@naver.com>
2022-09-08 16:47:15 -0700

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "",
+    redirect: "/page",
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/page",
+    component: () => import("@/views/Home/Index.vue"),
+    children: [],
+  },
+  {
+    path: "*",
+    name: "404",
+    component: () => import("@/views/404.vue"),
   },
 ];
 
---
+++ src/views/Home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view />
+    <footer-container />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import FooterContainer from "@/containers/Common/FooterContainer.vue";
+
+@Component({
+  components: {
+    FooterContainer,
+  },
+})
+export default class extends Vue {}
+</script>
+
+<style scoped></style>
